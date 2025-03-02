1fecf52ad7d2c9daa67a4155be05066db90f3112
navid bahrami <navidbahrami1996@gmail.com>
2025-03-02 13:54:51 +0330

    add variants

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -2,10 +2,16 @@ import { Button, Stack, TextField } from "@mui/material"
 
 const Auth = () => {
     return(
-    <Stack spacing={2} sx={{height : "100vh" , maxWidth: "40%" , margin : "0 auto" , justifyContent : "center"}}>
-      <TextField type="email" label="EMAIL"/>
-      <TextField type="password" label="PASSWORD"/>
-      <Button>LOGIN</Button>
+    <Stack 
+    spacing={2} 
+    sx={{
+      height : "100vh" ,
+      maxWidth: {xs : "80%" , md : "40%"} ,
+      margin : "0 auto" ,
+      justifyContent : "center"}}>
+      <TextField type="email" label="EMAIL" variant="standard"/>
+      <TextField type="password" label="PASSWORD" variant="standard"/>
+      <Button variant="text">LOGIN</Button>
     </Stack>
   )
 }
