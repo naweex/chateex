b066c7fa02c29cb1827dcba79f14168bf32023d3
navid bahrami <navidbahrami1996@gmail.com>
2025-03-04 23:52:47 +0330

    add some fields

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,6 +1,10 @@
 import { Button, Stack, TextField } from "@mui/material"
+import { useState } from "react";
 
 const Auth = () => {
+  const [email , setEmail] = useState("")
+  const [password , setPassword] = useState("")
+
     return(
     <Stack 
     spacing={2} 
@@ -9,8 +13,8 @@ const Auth = () => {
       maxWidth: {xs : "80%" , md : "40%"} ,
       margin : "0 auto" ,
       justifyContent : "center"}}>
-      <TextField type="email" label="EMAIL" variant="standard"/>
-      <TextField type="password" label="PASSWORD" variant="standard"/>
+      <TextField type="email" label="EMAIL" variant="standard" value={email} onChange={(event) => setEmail(event.target.value)}/>
+      <TextField type="password" label="PASSWORD" variant="standard" value={password} onChange={(event) => setPassword(event.target.value)}/>
       <Button variant="text">LOGIN</Button>
     </Stack>
   )
