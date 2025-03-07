ae4edba801fac7e69241a134331527cfcf559186
navid bahrami <navidbahrami1996@gmail.com>
2025-03-07 19:10:39 +0330

    some changes

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,11 +1,12 @@
 import { Button, Stack, TextField } from "@mui/material"
-import { useState } from "react";
+import React, { useState } from "react";
 
 interface AuthProps {
   submitLabel : string;
-  onSubmit : (credentials : {email : string , password : string}) => Promise<void>
+  onSubmit : (credentials : {email : string , password : string}) => Promise<void>;
+  children : React.ReactNode;
 }
-const Auth = ({submitLabel , onSubmit} : AuthProps) => {
+const Auth = ({submitLabel , onSubmit , children} : AuthProps) => {
   const [email , setEmail] = useState("")
   const [password , setPassword] = useState("")
 
@@ -19,7 +20,10 @@ const Auth = ({submitLabel , onSubmit} : AuthProps) => {
       justifyContent : "center"}}>
       <TextField type="email" label="EMAIL" variant="standard" value={email} onChange={(event) => setEmail(event.target.value)}/>
       <TextField type="password" label="PASSWORD" variant="standard" value={password} onChange={(event) => setPassword(event.target.value)}/>
-      <Button variant="text" onClick={() => onSubmit({email , password})}>{submitLabel}</Button>
+      <Button variant="text" onClick={() => onSubmit({email , password})}>
+      {submitLabel}
+      </Button>
+      {children}
     </Stack>
   )
 }
