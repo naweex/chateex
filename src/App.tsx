a665aa986d65d38170e71af82531a7c38a2cacfc
navid bahrami <navidbahrami1996@gmail.com>
2025-03-07 13:08:50 +0330

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import Auth from "./components/auth/Auth";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/routes";
 
 const darkTheme = createTheme({
   palette : {
@@ -11,7 +12,7 @@ const App = () => {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
       <Container>
-      <Auth/>
+      <RouterProvider router={router} />
       </Container>
     </ThemeProvider>
   )
